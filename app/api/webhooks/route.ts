d6e9d2daa1cbbd137b1fa86cf1b645125732d0c6
adriendelagneau <delagneauadrien@yahoo.fr>
2024-02-22 13:37:04 +0100

    first commit

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,16 +1,17 @@
 import { updateUserStatus } from "@/actions/userActions";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 //import User from "@/models/User";
 
 
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
-const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "")
+const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET || ""
+
 // Stripe requires the raw body to construct the event.
 
 
-const updateUserSubscription = async (session) => {
+const updateUserSubscription = async (session: any) => {
 
     const customerEmail = session.customer_email
     const subscriptionNumber = session.id
@@ -24,20 +25,19 @@ const updateUserSubscription = async (session) => {
 }
 
 
-export const POST = async (req, res) => {
-console.log('toto')
-    const text = await req.text();
-    const sig = req.headers.get("stripe-signature")/// work
+export const POST = async (req: NextRequest, res: NextResponse) => {
 
+    const text = await req.text();
+    const sig = req.headers.get("stripe-signature") || ""
 
     let event 
+
+
     try {
         event = stripe.webhooks.constructEvent(text, sig, webhookSecret)
     } catch (err) {
         // On error, log and return the error message
-        console.log(`❌ Error message: ${err.message}`)
-        res.status(400).send(`Webhook Error: ${err.message}`)
-        return
+        return NextResponse.error();
     }
     console.log("received ", event.type);
 
@@ -45,10 +45,10 @@ console.log('toto')
     if(event.type === 'checkout.session.completed'){
         const session = event.data.object
          updateUserSubscription(session)
-        let schedule = await stripe.subscriptionSchedules.create({
-            from_subscription: session.subscription
-        })
-        console.log(`Schedule created: ${schedule.id}`)
+        // let schedule = await stripe.subscriptionSchedules.create({
+        //     from_subscription: session.subscription
+        // })
+        // console.log(`Schedule created: ${schedule.id}`)
     }
     // Successfully constructed event
     return NextResponse.json({ succes: "xx" });
